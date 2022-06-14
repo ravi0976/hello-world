02c8e52286b99031eb47fd498caf12e05035bf77
ravi0976 <107473800+ravi0976@users.noreply.github.com>
2022-06-14 16:46:35 +0530

    Update App.js

--- Downloads/hello-world/src/App.js
+++ Downloads/hello-world/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-   (
-    <div className="">
+function App() {http://192.168.4.239:8080/
+   return(
+    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo/>
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           hello world
         </p>
